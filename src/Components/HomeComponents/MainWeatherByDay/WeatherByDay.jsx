a1d57a488634dd89cbba5557c829d77073ce671d
Beqael <beqaelashvili3@gmail.com>
2024-05-27 20:02:25 +0400

    add_Weathers+by+day

--- src/Components/HomeComponents/MainWeatherByDay/WeatherByDay.jsx
+++ src/Components/HomeComponents/MainWeatherByDay/WeatherByDay.jsx
@@ -1,11 +1,64 @@
 import { useContext, useEffect, useState } from "react";
 import { GlobalContext } from "@src/Providers/GlobalContext";
 
+const daysOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
 export function WeatherByDay() {
-  const { TbilisiWeather, currentWeekDay } = useContext(GlobalContext);
+  const { TbilisiWeather } = useContext(GlobalContext);
+  const [currentDay, setCurrentDay] = useState(new Date());
+  const current = currentDay.getDay();
+
+  const reorderedDaysOfWeek = [
+    ...daysOfWeek.slice(current),
+    ...daysOfWeek.slice(0, current),
+  ];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentDay(new Date());
+    }, 60000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div>
-      <h1>{currentWeekDay}</h1>
+      <div>
+        <div className="flex gap-4">
+          {reorderedDaysOfWeek.map((day, index) => {
+            return (
+              <div key={index}>
+                <p>{day}</p>
+                {TbilisiWeather !== undefined && (
+                  <>
+                    <div>
+                      <div>
+                        {TbilisiWeather.forecast.forecastday[index].date}
+                      </div>
+                    </div>
+                  </>
+                )}
+              </div>
+            );
+          })}
+        </div>
+        <div>
+          {TbilisiWeather !== undefined && (
+            <>
+              {TbilisiWeather.forecast.forecastday.map((item, index) => {
+                return <div key={index}></div>;
+              })}
+            </>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
