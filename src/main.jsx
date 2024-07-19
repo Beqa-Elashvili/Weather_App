8be6cad2f2e51d721f87d0e7abd98060192e9ff1
Beqael <beqaelashvili3@gmail.com>
2024-07-19 17:05:51 +0400

    corection

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { GlobalProvider } from "./Providers/GlobalContext.jsx";
 import { BrowserRouter } from "react-router-dom";
-import "./assets/Globalcss.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
